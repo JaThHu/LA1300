d36cc403b04be98cf67d3f6a3c13f09b2e69c622
JaThHu <111045598+JaThHu@users.noreply.github.com>
2023-09-06 08:31:09 +0200

    Add files via upload

---
+++ LA1300_Zahlenratspiel/Numbergenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA1300_Zahlenratspiel
+{
+    internal class Numbergenerator
+    {    
+        public int RnmbG()
+        {
+
+            int ranu;
+            Random random = new Random();
+            ranu = random.Next(1, 101);
+            
+            return ranu; 
+        }
+        
+    }
+}
---
+++ LA1300_Zahlenratspiel/Ueberpruefung.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LA1300_Zahlenratspiel
+{
+    internal class Ueberpruefung
+    {
+        public void begruessung()
+        {
+            Console.WriteLine("Willkommen beim Zahlenspiel!");
+            Console.WriteLine("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Versuche sie zu erraten.");
+        }
+        public void kontrollieren()
+        {
+            bool nochmal;
+            do
+            {
+                nochmal = false;
+                Numbergenerator numbergenerator = new Numbergenerator();
+                int zufallszahl = numbergenerator.RnmbG();
+
+                int versuche = 0;
+                bool geraten = false;
+
+                while (!geraten)
+                {
+                    Console.Write("Gib deine Vermutung ein: ");
+
+                    if (int.TryParse(Console.ReadLine(), out int vermutung))
+                    {
+                        versuche++;
+
+                        if (vermutung < zufallszahl)
+                        {
+                            Console.WriteLine("Die gesuchte Zahl ist größer.");
+                        }
+                        else if (vermutung > zufallszahl)
+                        {
+                            Console.WriteLine("Die gesuchte Zahl ist kleiner.");
+                        }
+                        else
+                        {
+                            geraten = true;
+                            Console.WriteLine($"Glückwunsch! Du hast die Zahl {zufallszahl} in {versuche} Versuchen erraten.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe. Bitte gib eine ganze Zahl ein.");
+                    }
+
+                }
+
+                
+                Console.WriteLine("Willst du nochmal spielen? ");
+                string nochmalEingabe = Console.ReadLine().ToUpper();
+                if( nochmalEingabe == "JA")
+                {
+                    nochmal = true;
+                }
+
+            } while (nochmal == true);
+            Environment.Exit(0);
+        }
+
+    }
+}
